398fe0fc4daa09c940aaf792088a5fcac6fe276b
Jessica Moreira <jessicamoreira51858@gmail.com>
2023-09-25 15:20:49 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://vitamaresias.com.br/wp-content/uploads/2023/02/saude-mental-e-qualidade-de-vida.jpg",
+    "title": "Saúde e qualidade de vida",
+    "description":"Donec vitae odio vitae odio semper facilisis ac non dolor. Ut ut nulla ligula. Aliquam non est purus. Vivamus eleifend tempus fermentum. Etiam diam ligula, laoreet sit amet velit ac, molestie efficitur nisi. Maecenas malesuada ipsum purus, eget ultricies massa rhoncus sed. Integer dignissim diam nec nisl consectetur interdum."
+  },
+  {
+    "id":"2",
+    "photo":"https://assets-br.wemystic.com.br/20210125152605/Flor-da-Vida-%E2%80%93-a-Geometria-Sagrada-da-luz.jpg",
+    "title": "novo artigo",
+    "description":"Aenean vulputate sapien consequat lectus placerat aliquam. Quisque nec nunc quis massa congue"
+  },
+  {
+    "id":"3",
+    "photo":"https://assets-br.wemystic.com.br/20210125155503/Geometria-Sagrada-o-alfabeto-do-universo.jpg",
+    "title": "novo artigo",
+    "description":"Curabitur eu velit ut massa egestas aliquam in sit amet nisi. Integer justo dui, placerat."
+  },
+  {
+    "id":"4",
+    "photo":"https://thumbs.dreamstime.com/b/vuelta-de-la-geometr%C3%ADa-sagrada-53320245.jpg",
+    "title": "novo artigo",
+    "description":"Donec tristique tincidunt mollis. Morbi tincidunt tortor at leo vestibulum elementum. Donec efficitur nibh in molestie convallis. Sed posuere quis tellus"
+  }
+]
